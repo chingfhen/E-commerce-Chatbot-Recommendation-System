4de5efe68592a89e9e62b7f84ffff89da1ed384f
Ching Fhen Tan <CTAN203@e.ntu.edu.sg>
2023-03-28 12:28:01 +0800

    Massive Update
    Session Recommendations. Telegram recommendations

--- src/main/api/fastapi/bot_world_classes/item_classes.py
+++ src/main/api/fastapi/bot_world_classes/item_classes.py
@@ -9,3 +9,24 @@ class Product(BaseModel):
     product_name: str
     categories: str
     image_url: str
+
+"""
+Query format for sending a recommendation
+"""
+class TelegramRecommendation(BaseModel):
+    chat_id: int
+    item: Product
+
+
+if __name__ == "__main__":
+    product = Product(product_id = 23826146098, 
+                    product_name = "a product name", 
+                    categories = "a product category", 
+                    image_url = "https://cf.shopee.sg/file/sg-11134207-23020-9nqha14zvsnvf4")
+    print("Created Product: ")
+    print(product)
+
+    telegram_recommendation = TelegramRecommendation(chat_id = 123, 
+                                    item = product)
+    print("Created TelegramRecommendQuery: ")
+    print(telegram_recommendation)
--- src/main/api/fastapi/bot_world_classes/query_classes.py
+++ src/main/api/fastapi/bot_world_classes/query_classes.py
@@ -1,11 +1,66 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from typing import Optional
 
 
 """
-Input to get recommendation
+Request format for recommend endpoint
 """
-class Query(BaseModel):
-    user_id: str
-    top_k: Optional[int] = 1
-    chat_id: Optional[int] = None
+class RecommendRequest(BaseModel):
+    
+    user_id: Optional[str] = None
+    item_id: Optional[str] = None
+    by: Optional[str] = "popularity"
+                                
+    class Config:
+        allow_population_by_field_name = True
+        validate_assignment = True
+        arbitrary_types_allowed = True
+
+    """
+    Valids the inputs
+    notes:
+        1. user_id and item_id are first assigned without checking.
+        2. during validation of "by", perform check using check_request_validity
+
+    """
+    @validator('by')
+    def check_request_validity(cls, value, values):
+        # only these recommendation types allowed
+        allowed_values = ["user", "item", "popularity", "hybrid"]
+        if value not in allowed_values:
+            raise ValueError(f"Invalid value for 'by': {value}. Allowed values are {allowed_values}.")
+        # when recommending by item, item_id must be given
+        if value == "item" and values.get("item_id") is None:
+            raise ValueError(f"Item ID is required for 'by':'item'")
+        # when recommending by user, user_id is required
+        if value == "user" and values.get("user_id") is None:
+            raise ValueError(f"User ID is required for 'by':'user'")
+        # when recommending by hybrid, user_id is required
+        if value == "hybrid" and values.get("user_id") is None:
+            raise ValueError(f"User ID is required for 'by':'hybrid'")
+        return value
+
+if __name__ == "__main__":
+    # Catch invalid cases
+    try:
+        recommend_request = RecommendRequest(
+            by="hybrid"
+        )
+    except ValueError as e:
+        print(e)
+        pass
+    try:
+        recommend_request = RecommendRequest(
+            by="item"
+        )
+    except ValueError as e:
+        print(e)
+        pass
+    try:
+        recommend_request = RecommendRequest(
+            by="user"
+        )
+    except ValueError as e:
+        print(e)
+        pass
+        

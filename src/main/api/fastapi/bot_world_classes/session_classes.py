17d6e55621c7e5181db0900d99217e301bce7439
Ching Fhen Tan <CTAN203@e.ntu.edu.sg>
2023-04-01 12:39:29 +0800

    SessionRecommendations
    from deque to cycle

--- src/main/api/fastapi/bot_world_classes/session_classes.py
+++ src/main/api/fastapi/bot_world_classes/session_classes.py
@@ -1,37 +1,33 @@
 from typing import List, Optional
-from collections import deque
-
+from itertools import cycle
 
 
 """
 Stores a set of recommendations for each unique ID
 notes:
-this optimizes system i.e. reduces model and database calls 
-Uses FIFO Data structure for further optimization
+Cycles through the set of recommendations with itertools.cycle
 """
 class SessionRecommendations(object):
     def __init__(self):
         self.session = dict()
     """
-    Checks that 1. Session recommendations for ID exists 2. and is not empty
+    Checks that Session recommendations for ID exists 
     """
     def exists(self, ID: str):
-        return (self.session.get(str(ID)) is not None) and (len(self.session[str(ID)])>0)
+        return self.session.get(str(ID)) is not None
     """
     Add new session - add new set of recommendations
     """
     def add(self, ID: str, recommendations: List):
-        self.session[str(ID)] = deque(recommendations)
+        self.session[str(ID)] = cycle(recommendations)
         return
     """
     Make 1 recommendation for unique ID
     """
     def recommend(self, ID: str):  
-        try:
-            item = self.session[str(ID)].popleft()
-        except IndexError:
-            raise Exception("Session does not exist. Please add a new session with add()")
-        return item
+        assert self.exists(str(ID))
+        return next(self.session[str(ID)])
+
 
 if __name__=="__main__":
     session = SessionRecommendations()
